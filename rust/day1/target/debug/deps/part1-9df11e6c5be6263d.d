5558840922917429d9140837bab223190192720a
apesteguia <mikeltxomikeltxo@gmail.com>
2024-12-17 13:21:32 +0100

    default packages

---
+++ rust/day1/target/debug/deps/part1-9df11e6c5be6263d.d
@@ -0,0 +1,5 @@
+/home/mikel/Escritorio/programacion/aoc2024/rust/day1/target/debug/deps/libpart1-9df11e6c5be6263d.rmeta: src/bin/part1.rs
+
+/home/mikel/Escritorio/programacion/aoc2024/rust/day1/target/debug/deps/part1-9df11e6c5be6263d.d: src/bin/part1.rs
+
+src/bin/part1.rs:
---
+++ rust/day1/target/debug/deps/part1-b57725039db305c2.d
@@ -0,0 +1,5 @@
+/home/mikel/Escritorio/programacion/aoc2024/rust/day1/target/debug/deps/part1-b57725039db305c2: src/bin/part1.rs
+
+/home/mikel/Escritorio/programacion/aoc2024/rust/day1/target/debug/deps/part1-b57725039db305c2.d: src/bin/part1.rs
+
+src/bin/part1.rs:
